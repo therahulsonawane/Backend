246dba3534cf3f777b18f4a9a4c839a29baa4643
therahulsonawane <rahulsonwane7066@gmail.com>
2024-09-02 10:50:13 +0530

    user gnerated

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
-// const MONGODB_URI = "mongodb+srv://rahul:rahul@backend.uu6wx.mongodb.net";
 
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI / DB_NAME}`
+      `${process.env.MONGODB_URI}/${DB_NAME}`
     );
     console.log(
       `\n MOngoDB connected !! DB Host: ${connectionInstance.connection.host}`
--- src/index.js
+++ src/index.js
@@ -6,7 +6,20 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then((result) => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port: ${process.env.PORT}`);
+    });
+
+    app.on("error", (error) => {
+      console.log("ERR: ", error);
+      throw error;
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB Connection failed!!", err);
+  });
 
 /*
 import express from "express";
